4a127abd9b4ad9d3800b07b524415de05f00dad7
h-dew <benjaminscown@gmail.com>
2026-04-09 14:16:36 -0700

    edit profile finished

--- app/src/main/java/com/example/cosc341_bitg/HomeActivity.java
+++ app/src/main/java/com/example/cosc341_bitg/HomeActivity.java
@@ -27,12 +27,16 @@ public class HomeActivity extends AppCompatActivity {
 
         Button btnMatches = findViewById(R.id.btnMatches);
         Button btnMessages = findViewById(R.id.btnMessages);
+        Button btnProfile = findViewById(R.id.btnProfile);
 
         btnMatches.setOnClickListener(v ->
                 startActivity(new Intent(this, MatchFeedActivity.class)));
 
         btnMessages.setOnClickListener(v ->
                 startActivity(new Intent(this, InboxActivity.class)));
+
+        btnProfile.setOnClickListener(v ->
+                startActivity(new Intent(this, EditProfileActivity.class)));
     }
 
     @Override
